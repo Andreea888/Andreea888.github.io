0d8ad5b5d79d670835e523e7f3a0ff04d6b6e573
Rusu Andreea <rusuandreea888@gmail.com>
2025-08-07 17:55:52 +0300

    added sounds to lg and mute buttons

--- src/text.js
+++ src/text.js
@@ -7,6 +7,11 @@ const workBody = document.getElementById('work-body');
 const aboutTitle = document.getElementById('about-title');
 const aboutBody = document.getElementById('about-body');
 
+var click = new Howl({
+ src: ["./music/click.ogg"],
+ loop: false,
+ volume: 1,
+});
 
 function setLanguage(lang) {
   workTitle.textContent = translations[lang].workTitle;
@@ -21,12 +26,36 @@ setLanguage('en');
 const isMobile = navigator.userAgent.includes("Mobi");
 
 if(!isMobile){
-  document.getElementById('btn-en').addEventListener('click', () => setLanguage('en'));
-  document.getElementById('btn-ro').addEventListener('click', () => setLanguage('ro'));
-  document.getElementById('mute').addEventListener('click', () => muteMusic());
+  document.getElementById('btn-en').addEventListener('click', 
+    () => {
+      setLanguage('en')
+      click.play();
+    });
+  document.getElementById('btn-ro').addEventListener('click', 
+    () => {
+      setLanguage('ro')
+      click.play();
+    });
+  document.getElementById('mute').addEventListener('click', 
+    () => {
+      muteMusic()
+      click.play();
+    });
 }
 else{
-  document.getElementById('btn-en').addEventListener('touchend', () => setLanguage('en'));
-  document.getElementById('btn-ro').addEventListener('touchend', () => setLanguage('ro'));
-  document.getElementById('mute').addEventListener('touchend', () => muteMusic());
+  document.getElementById('btn-en').addEventListener('touchend', 
+    () => {
+      setLanguage('en')
+      click.play();
+    });
+  document.getElementById('btn-ro').addEventListener('touchend', 
+    () => {
+      setLanguage('ro')
+      click.play();
+    });
+  document.getElementById('mute').addEventListener('touchend', 
+    () => {
+      muteMusic()
+      click.play();
+    });
 }
